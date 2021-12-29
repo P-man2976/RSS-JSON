07df672e4c547978874753d809202a6274d5fd32
ぴーまん <70518694+P-man2976@users.noreply.github.com>
2021-12-29 14:22:56 +0900

    211229 1422

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { XMLParser } = require("fast-xml-parser");
+const moment = require("moment");
 const fetch = require("cross-fetch");
 const http = require("http");
 const dotenv = require("dotenv");
@@ -22,7 +23,7 @@ let counter = {
 
 // RSSからJSONへの変換URLへのリクエスト
 app.get("/api/rss-json", async (req, res) => {
-	console.log("Request received");
+	console.log(`[${makeISOTimeString()}]Request received`);
 	const start = new Date();
 	const requestUrl = req.query.req_url;
 
@@ -38,9 +39,9 @@ app.get("/api/rss-json", async (req, res) => {
 			// ステータス200でレスポンスを送信
 			res.status(200).send(rssJson);
 			const end = new Date();
-			console.log(`Request processed in ${end - start}ms`);
+			console.log(`[${makeISOTimeString()}]Request processed in ${end - start}ms`);
 			counter.success++;
-			console.log(`Access count : ${counter.success}`);
+			console.log(`[${makeISOTimeString()}]Access count : ${counter.success}`);
 		} catch (error) {
 			console.error(error);
 			res.status(500).send();
@@ -49,11 +50,11 @@ app.get("/api/rss-json", async (req, res) => {
 		}
 	} else {
 		// XML(RSS)を取得するURLのパラメータが指定されていない場合
-		console.log("Request URL undefined");
+		console.log(`[${makeISOTimeString()}]Request URL undefined`);
 		// ステータス400でレスポンスを送信
 		res.status(400).send("Request URL undefined");
 		const end = new Date();
-		console.log(`Request processed in ${end - start}ms`);
+		console.log(`[${makeISOTimeString()}]Request processed in ${end - start}ms`);
 		counter.error++;
 	}
 });
@@ -70,15 +71,15 @@ app.delete("/api/reset", async (req, res) => {
 
 			if (queryScope.success !== -1) {
 				counter.success = 0;
-				console.log("Reset success counter");
+				console.log(`[${makeISOTimeString()}]Reset success counter`);
 			}
 			if (queryScope.fail !== -1) {
 				counter.fail = 0;
-				console.log("Reset fail counter");
+				console.log(`[${makeISOTimeString()}]Reset fail counter`);
 			}
 			if (queryScope.error !== -1) {
 				counter.error = 0;
-				console.log("Reset error counter");
+				console.log(`[${makeISOTimeString()}]Reset error counter`);
 			}
 			res.status(200).send();
 		} else {
@@ -91,5 +92,9 @@ app.delete("/api/reset", async (req, res) => {
 });
 
 server.listen(port, async () => {
-	console.log(`Server listening on port ${port}`);
+	console.log(`[${makeISOTimeString()}]Server listening on port ${port}`);
 });
+
+const makeISOTimeString = () => {
+	return moment().tz("Asia/Tokyo").format();
+};
